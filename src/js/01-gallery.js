6e9aaa9600a328804a21bd4213ebfaf2b7deffe5
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-15 22:39:46 +0300

    fix errors

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,4 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(galleryItems);
+
