6f044c9aa1f2ade019e8f4ec5ee0078e7b311326
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-16 00:21:49 +0300

    finish task-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,25 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const getGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" 
+         src="${preview}" 
+         alt="${description}" />
+      </a>
+   </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', getGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
